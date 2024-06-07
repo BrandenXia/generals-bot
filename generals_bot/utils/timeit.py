90afa4a1cc4b441bc2c68f4196eddeff144236c8
BrandenXia <xxtbranden@outlook.com>
2024-06-07 09:45:00 -0700

    perf(utils/timeit): simplify function logic

--- generals_bot/utils/timeit.py
+++ generals_bot/utils/timeit.py
@@ -9,20 +9,14 @@ logger = logging.getLogger(__name__)
 
 
 def timeit[FuncType: Callable](func: FuncType) -> FuncType:
-    if is_async:
+    """Decorator to measure the time taken by a function to execute"""
 
-        async def wrapper(*args, **kwargs):
-            start = time.time()
-            result = await func(*args, **kwargs)
-            logger.debug(f"{func.__name__} took {time.time() - start} seconds")
-            return result
-
-    else:
-
-        def wrapper(*args, **kwargs):
-            start = time.time()
-            result = func(*args, **kwargs)
-            logger.debug(f"{func.__name__} took {time.time() - start} seconds")
-            return result
+    async def wrapper(*args, **kwargs):
+        start = time.time()
+        result = (
+            (await func(*args, **kwargs)) if is_async(func) else func(*args, **kwargs)
+        )
+        logger.debug(f"{func.__name__} took {time.time() - start} seconds")
+        return result
 
     return wrapper
